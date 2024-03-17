ee799ba4b91dc6675713c278657af3309fbd5844
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2024-03-17 17:39:29 +0600

    update web route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/migrate', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate:fresh');
+    return \Illuminate\Support\Facades\Artisan::output();
 });
 Route::get('',[\App\Http\Controllers\WebsiteController::class,'home'])->name('home');
+
